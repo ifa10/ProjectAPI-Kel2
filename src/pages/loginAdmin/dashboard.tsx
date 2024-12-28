b71ad3e78c8fa80dd7b06a34b8dc3bb59fcd202a
uhanik148 <bangwool148@gmail.com>
2024-12-28 20:01:38 +0700

    Update dashboard.tsx

--- src/pages/loginAdmin/dashboard.tsx
+++ src/pages/loginAdmin/dashboard.tsx
@@ -1,30 +1,30 @@
-// pages/dashboard.tsx
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+// // pages/dashboard.tsx
+// import { useEffect, useState } from 'react';
+// import { useRouter } from 'next/router';
 
-const Dashboard = () => {
-  const [message, setMessage] = useState('');
-  const router = useRouter();
+// const Dashboard = () => {
+//   const [message, setMessage] = useState('');
+//   const router = useRouter();
 
-  useEffect(() => {
-    const fetchDashboard = async () => {
-      const token = localStorage.getItem('token');
-      const response = await fetch('/api/dashboard', {
-        headers: { Authorization: `Bearer ${token}` },
-      });
+//   useEffect(() => {
+//     const fetchDashboard = async () => {
+//       const token = localStorage.getItem('token');
+//       const response = await fetch('/api/dashboard', {
+//         headers: { Authorization: `Bearer ${token}` },
+//       });
 
-      if (response.ok) {
-        const data = await response.json();
-        setMessage(data.message);
-      } else {
-        router.push('/'); // Jika tidak diizinkan, arahkan ke login
-      }
-    };
+//       if (response.ok) {
+//         const data = await response.json();
+//         setMessage(data.message);
+//       } else {
+//         router.push('/'); // Jika tidak diizinkan, arahkan ke login
+//       }
+//     };
 
-    fetchDashboard();
-  }, [router]);
+//     fetchDashboard();
+//   }, [router]);
 
-  return <div>{message}</div>;
-};
+//   return <div>{message}</div>;
+// };
 
-export default Dashboard;
+// export default Dashboard;
